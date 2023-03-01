121f3611b12c279a63dc375c8bc48897c3f205d8
Thomas Calle <thos.calle@gmail.com>
2023-03-01 14:35:30 -0500

    update

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const MyModel = require('../models/myModel');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
