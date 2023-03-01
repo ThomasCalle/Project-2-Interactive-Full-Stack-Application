9e322a27211cc8d4e3cde039cc47c4108d40294e
omgthegreenranger <das.shaggy@gmail.com>
2023-03-01 11:21:01 -0500

    First home route complete

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,30 @@
+const { User, Category, Event, Settings } = require('../models');
+const sequelize = require('sequelize');
+const router = require('express').Router();
+
+// Assume session includes all User ID and relevant settings info.
+
+// primary route
+// get Event Data, find all by session user ID foreign key
+//  - associate category table with each event
+//  - use helper to calculate T1, T2, T3 in to API
+
+router.get('/', async (req, res) => {
+    try {
+        const eventData = await Event.findAll({
+            attributes:['id', 'title', 'description', 'due_date'],
+            where: {
+                id: user_id
+            },
+            include: [{
+                model: Category,
+                attributes: ['name', 'type', 'T1', 'T2', 'T3']
+            }]
+        });
+        const events = eventData.map((event) => event.get({ plain: true }));
+        res.render('homepage', { events });  
+    } catch (err) {
+        res.render('login');
+    }
+});
+
