f0b5946ea204dc48303824e184c16c5461b09cb0
omgthegreenranger <das.shaggy@gmail.com>
2023-03-03 13:51:48 -0500

    fetchEvents function complete

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,6 @@
-var dayjs = require('dayjs');
+const dayjs = require('dayjs');
+
+
 async function fetchEvents() {
     const response =  await fetch('./event', {
         method: "GET",
@@ -8,26 +10,42 @@ async function fetchEvents() {
     });
     
     let eventFetch = await response.json();
-
     for(let i = 0; i < eventFetch.length; i++) {
 
-        let dueDate = dayjs(eventFetch.due_date, "MM-DD-YYYY")
-        console.log(dueDate);
+        // get Due Date and convert to unix time.
+        let dueDate = dayjs(eventFetch[i].due_date);
+        let dueDay = dayjs(dueDate).unix();
 
+
+        // Get the array of the thresholds
         let t1 = eventFetch[i].category.t1.split(" ");
         let t2 = eventFetch[i].category.t2.split(" ");
         let t3 = eventFetch[i].category.t3.split(" ");
-        t1 = dueDate.subtract(t1[0], t1[1]);
-        t2 = dueDate.subtract(t2[0], t2[1]);
-        t3 = dueDate.subtract(t3[0], t3[1]);
+        
+        // Get date of thresholds as UNIX time
+        t1date = dueDate.subtract(t1[0], t1[1]).unix();
+        t2date = dueDate.subtract(t2[0], t2[1]).unix();
+        t3date = dueDate.subtract(t3[0], t3[1]).unix();
 
-        eventFetch[i].t1 = t1;
-        eventFetch[i].t2 = t2;
-        eventFetch[i].t3 = t3;
+        // Get the duration in unix code
+        t1 = dueDay - t1date;
+        t2 = dueDay - t2date;
+        t3 = dueDay - t3date;
+
+        // calculate timeFrame with t3 as outer field, divide by nine for unix duration of each threshold.
+        let timeFrame = t3;
+        timeFrame = Math.floor(timeFrame / 9);
+        
+        // Add unix dates of thresholds back to array., along with threshold marker
+        eventFetch[i].t1Date = t1date;
+        eventFetch[i].t2Date = t2date;
+        eventFetch[i].t3Date = t3date;
+        eventFetch[i].t1 = Math.floor(t1 / timeFrame );
+        eventFetch[i].t2 = Math.floor(t2 / timeFrame );
+        eventFetch[i].t3 = Math.floor(t3 / timeFrame );
     }
-    console.log(eventFetch);
     
     return eventFetch;
 }
 
-fetchEvents();
+// fetchEvents();
