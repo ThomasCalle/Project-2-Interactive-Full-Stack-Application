00c2d5506801b0c32c4ef7672a48306d9a936dc5
omgthegreenranger <das.shaggy@gmail.com>
2023-03-04 15:51:14 -0500

    Update with nowTime data in eventTime

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,7 @@ const dayjs = require('dayjs');
 
 module.exports = {
     calculateThresholdsDates: (eventData) => {
-
+        //okay, last 
         let dueDate = dayjs(eventData.due_date)
 
         let t1 = eventData.category.t1.split(" ");
@@ -27,7 +27,7 @@ module.exports = {
             // get Due Date and convert to unix time.
             let dueDate = dayjs(eventData[i].due_date);
             let dueDay = dayjs(dueDate).unix();
-    
+            let nowDay = dayjs().unix();
     
             // Get the array of the thresholds
             let t1 = eventData[i].category.t1.split(" ");
@@ -38,12 +38,15 @@ module.exports = {
             t1date = dueDate.subtract(t1[0], t1[1]).unix();
             t2date = dueDate.subtract(t2[0], t2[1]).unix();
             t3date = dueDate.subtract(t3[0], t3[1]).unix();
-    
+
             // Get the duration in unix code
             t1 = dueDay - t1date;
             t2 = dueDay - t2date;
             t3 = dueDay - t3date;
             const total = t1+t2+t3;
+
+            let nowDate = dueDay - nowDay;
+
             // calculate timeFrame with t3 as outer field, divide by nine for unix duration of each threshold.
             const bargraphLength = 9
             let timeFrame = t3;
@@ -53,16 +56,13 @@ module.exports = {
             eventData[i].t1Date = t1date;
             eventData[i].t2Date = t2date;
             eventData[i].t3Date = t3date;
-            eventData[i].t1 = (t1 / timeFrame );
-            eventData[i].t2 = (t2 / timeFrame );
-            eventData[i].t3 = (t3 / timeFrame );
             eventData[i].t1pct = t1 / total * bargraphLength;
             eventData[i].t2pct = t2 / total * bargraphLength;
             eventData[i].t3pct = t3 / total * bargraphLength;
-
+            eventData[i].nowTime = nowDate / timeFrame;
         }
         return eventData;
-    }
+    },
 
 
 }
