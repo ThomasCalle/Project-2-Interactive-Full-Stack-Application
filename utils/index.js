94b95cf7a5790f10d12f0ad2df63334fecf4a282
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-04 11:47:38 -0500

    updated bar graph to show stacked data based on timeframe

--- utils/index.js
+++ utils/index.js
@@ -1,37 +1,70 @@
 const dayjs = require('dayjs');
 
 module.exports = {
-    calculateThresholds: (eventData) => {
+    calculateThresholdsDates: (eventData) => {
 
-         let dueDate = dayjs(eventData.due_date)
+        let dueDate = dayjs(eventData.due_date)
 
-        // Get the array of the thresholds
-        let t1 = eventFetch[i].category.t1.split(" ");
-        let t2 = eventFetch[i].category.t2.split(" ");
-        let t3 = eventFetch[i].category.t3.split(" ");
+        let t1 = eventData.category.t1.split(" ");
+        let t2 = eventData.category.t2.split(" ");
+        let t3 = eventData.category.t3.split(" ");
+        t1 = dueDate.subtract(t1[0], t1[1]);
+        t2 = dueDate.subtract(t2[0], t2[1]);
+        t3 = dueDate.subtract(t3[0], t3[1]);
+
+        eventData.t1 = dayjs(t1).format("YYYY-MM-DD");
+        eventData.t2 = dayjs(t2).format("YYYY-MM-DD");
+        eventData.t3 = dayjs(t3).format("YYYY-MM-DD");
+        return eventData;
+    },
+
+
+
+    calculateThresholdBarPlots: (eventData) => {
         
-        // Get date of thresholds as UNIX time
-        t1date = dueDate.subtract(t1[0], t1[1]).unix();
-        t2date = dueDate.subtract(t2[0], t2[1]).unix();
-        t3date = dueDate.subtract(t3[0], t3[1]).unix();
+        for(let i = 0; i < eventData.length; i++) {
 
-        // Get the duration in unix code
-        t1 = dueDay - t1date;
-        t2 = dueDay - t2date;
-        t3 = dueDay - t3date;
+            // get Due Date and convert to unix time.
+            let dueDate = dayjs(eventData[i].due_date);
+            let dueDay = dayjs(dueDate).unix();
+    
+    
+            // Get the array of the thresholds
+            let t1 = eventData[i].category.t1.split(" ");
+            let t2 = eventData[i].category.t2.split(" ");
+            let t3 = eventData[i].category.t3.split(" ");
+    
+            // Get date of thresholds as UNIX time
+            t1date = dueDate.subtract(t1[0], t1[1]).unix();
+            t2date = dueDate.subtract(t2[0], t2[1]).unix();
+            t3date = dueDate.subtract(t3[0], t3[1]).unix();
+    
+            // Get the duration in unix code
+            t1 = dueDay - t1date;
+            t2 = dueDay - t2date;
+            t3 = dueDay - t3date;
+            const total = t1+t2+t3;
+            // calculate timeFrame with t3 as outer field, divide by nine for unix duration of each threshold.
+            const bargraphLength = 9
+            let timeFrame = t3;
+            timeFrame = timeFrame / bargraphLength;
+    
+            // Add unix dates of thresholds back to array., along with threshold marker
+            eventData[i].t1Date = t1date;
+            eventData[i].t2Date = t2date;
+            eventData[i].t3Date = t3date;
+            eventData[i].t1 = (t1 / timeFrame );
+            eventData[i].t2 = (t2 / timeFrame );
+            eventData[i].t3 = (t3 / timeFrame );
+            eventData[i].t1pct = t1 / total * bargraphLength;
+            eventData[i].t2pct = t2 / total * bargraphLength;
+            eventData[i].t3pct = t3 / total * bargraphLength;
 
-        // calculate timeFrame with t3 as outer field, divide by nine for unix duration of each threshold.
-        let timeFrame = t3;
-        timeFrame = Math.floor(timeFrame / 9);
-        
-        // Add unix dates of thresholds back to array., along with threshold marker
-        eventFetch[i].t1Date = t1date;
-        eventFetch[i].t2Date = t2date;
-        eventFetch[i].t3Date = t3date;
-        eventFetch[i].t1 = Math.floor(t1 / timeFrame );
-        eventFetch[i].t2 = Math.floor(t2 / timeFrame );
-        eventFetch[i].t3 = Math.floor(t3 / timeFrame );
+        }
+        return eventData;
     }
+
+
     
     return eventFetch;
 }
