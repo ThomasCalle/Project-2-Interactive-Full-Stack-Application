144428a415783e53894bf35763d4ad11109806d7
Stephen Cardie <das.shaggy@gmail.com>
2023-03-04 14:52:03 -0500

    Updating branch with fetch info

--- utils/helpers.js
+++ utils/helpers.js
@@ -19,9 +19,6 @@ module.exports = {
       } else {
         return `<span for="img" aria-label="gear">⚙️</span>`;
       }
-    },
-    // check_cat: () => {
-    //   return this.ariaPressed;
-    // },
+    }
   };
   
