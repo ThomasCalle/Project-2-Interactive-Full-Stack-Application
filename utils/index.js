8eefdd989847a8612a089295336d220e5d01af05
omgthegreenranger <das.shaggy@gmail.com>
2023-03-04 16:06:46 -0500

    Committed with nowTime.

--- utils/index.js
+++ utils/index.js
@@ -28,7 +28,6 @@ module.exports = {
             let dueDate = dayjs(eventData[i].due_date);
             let dueDay = dayjs(dueDate).unix();
             let nowDay = dayjs().unix();
-    
             // Get the array of the thresholds
             let t1 = eventData[i].category.t1.split(" ");
             let t2 = eventData[i].category.t2.split(" ");
@@ -38,7 +37,6 @@ module.exports = {
             t1date = dueDate.subtract(t1[0], t1[1]).unix();
             t2date = dueDate.subtract(t2[0], t2[1]).unix();
             t3date = dueDate.subtract(t3[0], t3[1]).unix();
-
             // Get the duration in unix code
             t1 = dueDay - t1date;
             t2 = dueDay - t2date;
@@ -46,12 +44,10 @@ module.exports = {
             const total = t1+t2+t3;
 
             let nowDate = dueDay - nowDay;
-
             // calculate timeFrame with t3 as outer field, divide by nine for unix duration of each threshold.
             const bargraphLength = 9
             let timeFrame = t3;
             timeFrame = timeFrame / bargraphLength;
-    
             // Add unix dates of thresholds back to array., along with threshold marker
             eventData[i].t1Date = t1date;
             eventData[i].t2Date = t2date;
@@ -59,7 +55,7 @@ module.exports = {
             eventData[i].t1pct = t1 / total * bargraphLength;
             eventData[i].t2pct = t2 / total * bargraphLength;
             eventData[i].t3pct = t3 / total * bargraphLength;
-            eventData[i].nowTime = nowDate / timeFrame;
+            eventData[i].nowTime = Math.ceil(nowDate / timeFrame);
         }
         return eventData;
     },
