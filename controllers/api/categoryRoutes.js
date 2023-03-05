a544554edb51d9e33d4224e674e6229b5fdbb09b
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-05 17:11:23 -0500

    added auto load of categories upon registration

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,9 +1,9 @@
 const { User, Category, Event } = require('../../models');
 const sequelize = require('sequelize');
 const router = require('express').Router();
+const withAuth = require('../../utils/auth');
 
-
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
         console.log(req.body.name);
         const categoryData = await Category.create({
@@ -11,7 +11,8 @@ router.post('/', async (req, res) => {
           type: req.body.type,
           t1: req.body.t1,
           t2: req.body.t2,
-          t3: req.body.t3
+          t3: req.body.t3,
+          user_id: req.session.userId
         });
         res.send(categoryData)
       } catch (err) {
