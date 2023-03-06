3470371cf30e6ca8cd9a3c1a7aa94fe7d3997fd2
Stefan Marinov <stefan_marinov@rogers.com>
2023-03-05 23:03:13 -0500

    reset fileds on modal close

--- public/js/updateScripts.js
+++ public/js/updateScripts.js
@@ -154,7 +154,33 @@ function categorySelects() {
   }
 };
 
-function whichPartial() {
-  document
+const resetFields = () => {
+  $('input[name="id"]').val('');
+  $('input[name="name"]').val('');
+  $('input[name="description"]').val('');
+  $('input[name="due_date"]').val('');
+  $(`select[name="category"]`).val('')
+
 }
 
+$("#closeBtnFooter").click(() => { resetFields()});
+$("#closeBtnHeader").click(() => { resetFields()});
+
+
+$("#editEvent").click(async (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  let eventData = await fetch(`/event/${event.target.dataset.id}`, {
+      method: "GET", 
+      headers: { 'Content-Type': 'application/json' },
+  });
+  eventData = await eventData.json();
+
+  $('input[name="id"]').val(eventData.id);
+  $('input[name="name"]').val(eventData.name);
+  $('input[name="description"]').val(eventData.description);
+  $('input[name="due_date"]').val(eventData.due_date);
+  $(`select[name="category"] option[value=${eventData.category.id}]`).attr('selected','selected');
+
+});
